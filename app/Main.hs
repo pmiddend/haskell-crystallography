2dad57934e29636ee3296fab6049686ee553fb00
Philipp Middendorf <philipp.middendorf@desy.de>
2021-08-27 11:21:06 +0200

    First SQL tries

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude(print, Either (Left, Right), putStrLn, Show (show), Foldable (length))
+import Prelude(print, Either (Left, Right), Show (show), Foldable (length), undefined)
+import Data.Text.IO(putStrLn)
+import Data.Text(pack, Text)
 import System.IO(IO)
 import Control.Applicative(pure)
-import Control.Monad(forM_)
+import Control.Monad(forM_, (=<<))
 import Data.ByteString(getContents)
-import Xtal.MTZ(parseMtz, mtzHeader, mtzHistory, mtzReflections)
+import Xtal.MTZ(parseMtz, mtzHeader, mtzHistory, mtzReflections, MtzFile, mtzTitle, mtzLocateHeader, mtzLocateHeaders, mtzDataset)
 import Data.Monoid((<>))
+import Data.Maybe(fromMaybe)
+import Data.Function((.))
+import Database.MySQL.Base(connect, query_, ciUser, ciPassword, ciDatabase, defaultConnectInfo)
+import qualified System.IO.Streams as Streams
+
+showText :: Show a => a -> Text
+showText = pack . show
+
+mtzdmp :: MtzFile -> IO ()
+mtzdmp mtz = do
+  putStrLn " * Title:"
+  putStrLn ""
+  putStrLn (" " <> fromMaybe "no title" (mtzLocateHeader mtzTitle mtz))
+  putStrLn ""
+  putStrLn (" * Number of Datasets = " <> showText (length (mtzLocateHeaders mtzDataset mtz)))
 
 main :: IO ()
 main = do
+  conn <- connect defaultConnectInfo { ciUser = "root", ciPassword = "root", ciDatabase = "SARS_COV_2_v2" }
+  (defs, is) <- query_ conn "SELECT crystal_id FROM Crystals"
+  print =<< Streams.toList is
+
+mtzmain :: IO ()
+mtzmain = do
   stdin <- getContents
   case parseMtz stdin of
-    Left e -> putStrLn e
-    Right v -> --putStrLn ("mtz read successful: " <> show (mtzHeader v) <> " header(s), " <> show (length (mtzHistory v)) <> " history(s): " <> show (mtzHistory v))
-      forM_ (mtzReflections v) print
+    Left e -> putStrLn (pack e)
+    Right v -> mtzdmp v
+      --forM_ (mtzReflections v) print
--- src/Xtal/MTZ.hs
+++ src/Xtal/MTZ.hs
@@ -1,34 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Xtal.MTZ (parseMtz, mtzHeader, mtzHistory, mtzReflections) where
+module Xtal.MTZ
+  ( parseMtz,
+    mtzHeader,
+    mtzHistory,
+    mtzReflections,
+    mtzLocateHeader,
+    mtzLocateHeaders,
+    mtzTitle,
+    mtzDataset,
+    MtzDatasetInfo,
+    MtzFile
+  )
+where
 
-import Data.String(String)
-import Data.Word(Word8)
-import Data.Bool((&&))
-import Data.Binary(decode, decodeOrFail)
-import Data.Binary.Get(runGetOrFail, getFloathost)
-import Data.Vector.Binary
 import Control.Applicative (Alternative (many), pure, (*>), (<*), (<*>), (<|>))
 import Control.Monad (mzero, replicateM, (>>=))
 import Data.Attoparsec.Binary (anyWord32le)
-import qualified Data.ByteString.Lazy as BSL
 import Data.Attoparsec.ByteString (IResult (Done, Fail, Partial), Parser, anyWord8, endOfInput, parse, parseOnly, skipWhile, string, take, takeWhile1)
 import qualified Data.Attoparsec.Text as AT
-import Data.ByteString (ByteString, dropWhileEnd, getContents, length,unpack)
+import Data.Binary (decode, decodeOrFail)
+import Data.Binary.Get (getFloathost, runGetOrFail)
+import Data.Bool ((&&))
+import Data.ByteString (ByteString, dropWhileEnd, getContents, length, unpack)
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as BSL
 import Data.Eq ((/=), (==))
 import Data.Function (id, (.))
 import Data.Functor (($>), (<$>), (<&>))
 import Data.Int (Int)
 import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
 import Data.Monoid ((<>))
+import Data.String (String)
 import qualified Data.Text as Text
-import Debug.Trace (traceShowId, trace)
-import Text.Show (Show (show))
-import Prelude (Bool (True), Char, Double, Either (Left, Right), Float, Functor (fmap), Maybe (Just, Nothing), MonadFail (fail), const, either, fromIntegral, read, undefined, (*), (+), (-))
 import qualified Data.Vector as BoxedVector
 import qualified Data.Vector.Unboxed as UnboxedVector
-
+import Data.Word (Word8)
+import Debug.Trace (trace, traceShowId)
+import Text.Show (Show (show))
+import Prelude (Bool (True), Char, Double, Either (Left, Right), Float, Functor (fmap), Maybe (Just, Nothing), MonadFail (fail), const, either, fromIntegral, read, undefined, (*), (+), (-))
 
 data MtzNcolInfo = MtzNcolInfo
   { mtzNcolColumnCount :: Int,
@@ -136,6 +146,10 @@ data MtzHeaderEntry
   | MtzHist Int
   | MtzUnknownHeader ByteString
   deriving (Show)
+
+mtzHeaderIsTitle :: MtzHeaderEntry -> Maybe ByteString
+mtzHeaderIsTitle (MtzTitle n) = Just n
+mtzHeaderIsTitle _ = Nothing
 
 mtzHeaderIsHist :: MtzHeaderEntry -> Maybe Int
 mtzHeaderIsHist (MtzHist n) = Just n
@@ -266,9 +280,11 @@ mtzHeaderEntryParser =
         <|> subParsedHeader "DWAVEL" dwavelParser MtzDwavel
         <|> (rawHeader "END" $> MtzEnd)
         <|> subParsedHeader "MTZHIST" (textSkipWs *> AT.decimal) MtzHist
+
 --        <|> MtzUnknownHeader <$> take 80
 
 magicHeaderLocation = 20
+
 mtzRecordSize = 80
 
 data MtzFile = MtzFile
@@ -276,6 +292,22 @@ data MtzFile = MtzFile
     mtzHeader :: [MtzHeaderEntry],
     mtzHistory :: [ByteString]
   }
+
+type MtzHeaderGetter a = MtzHeaderEntry -> Maybe a
+
+mtzLocateHeader :: MtzHeaderGetter a -> MtzFile -> Maybe a
+mtzLocateHeader f = listToMaybe . mapMaybe f . mtzHeader
+
+mtzLocateHeaders :: MtzHeaderGetter a -> MtzFile -> [a]
+mtzLocateHeaders f = mapMaybe f . mtzHeader
+
+mtzTitle :: MtzHeaderGetter Text.Text
+mtzTitle (MtzTitle n) = Just (bsToText n)
+mtzTitle _ = Nothing
+
+mtzDataset :: MtzHeaderGetter MtzDatasetInfo
+mtzDataset (MtzDataset n) = Just n
+mtzDataset _ = Nothing
 
 mtzParser = do
   -- Byte 1 to 4
@@ -294,8 +326,10 @@ mtzParser = do
   unused1 <- anyWord8
   -- Byte 12
   unused2 <- anyWord8
-  -- Manual says we start at byte 21 with the data, but actually, records are always 80 bytes, and we have the first record filled with some metadata,
-  -- so we skip ahead to the next record at byte 80
+  -- Manual says we start at byte 21 with the data, but actually,
+  -- records are always 80 bytes, and we have the first record filled
+  -- with some metadata, so we skip ahead to the next record at byte
+  -- 80
   let currentLocation = 12
   unused3 <- take (mtzRecordSize - currentLocation)
   rawData <- take (fromIntegral headerLocationInBytes - mtzRecordSize)
@@ -316,8 +350,6 @@ mtzParser = do
               unfolder = UnboxedVector.splitAt colCount
               unfoldedVector :: BoxedVector.Vector (UnboxedVector.Vector Float)
               unfoldedVector = BoxedVector.unfoldrExactN reflCount unfolder (UnboxedVector.fromList floatVector)
-              columnNames :: String
-              columnNames = show (Text.intercalate "," (mtzColName <$> mapMaybe mtzHeaderIsColInfo headerEntries))
-          pure (MtzFile (trace ("cols: " <> columnNames) unfoldedVector) headerEntries historyEntries)
+          pure (MtzFile unfoldedVector headerEntries historyEntries)
 
 parseMtz = parseOnly mtzParser
